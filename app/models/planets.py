eae752df3a70a898c7e937d9b93faf271ab70a61
luqi <lq.xie.work@gmail.com>
2024-10-18 11:57:58 -0700

    add Planet class and all instances

---
+++ app/models/planets.py
@@ -0,0 +1,16 @@
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+planets = [
+    Planet(1, "Mercury", "Smallest planet"),
+    Planet(2, "Venus", "Earths closet planet"),
+    Planet(3, "Earth", "It has 71 '%' of water"),
+    Planet(4, "Mars", "It's red and has life"),
+    Planet(5, "Jupiter", "It is a red planet"),
+    Planet(6, "Saturn", "It is a gas planet"),
+    Planet(7, "Uranus", "It is an ice planet"),
+    Planet(8, "Neptune", "It is farthest from the sun"),
+    Planet(9, "Pluto", "It's a dwarf planet")
+]
