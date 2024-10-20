fda8cc24ada7dc271960dba6ba2789f00f673b5d
luqi <lq.xie.work@gmail.com>
2024-10-19 21:37:45 -0700

    complete wave2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from ..models.planets import planets
 
-# endpoints
+# endpoint for reading all planets info
 planets_bp = Blueprint("planets_bp", __name__, url_prefix = "/planets")
 
-@planets_bp.get("")
+@planets_bp.get("/")
 def get_all_panets():
   result = []
 
@@ -15,3 +15,31 @@ def get_all_panets():
       description=planet.description
     ))
   return result
+
+# endpoint for reading one planet
+@planets_bp.get("/<planet_id>")
+def get_one_panet(planet_id):
+
+  planet = validate_planet(planet_id)
+  
+  return (dict(
+        id=planet.id,
+        name=planet.name,
+        description=planet.description))
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
+
+
