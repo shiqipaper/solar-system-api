4c9e5662fa8ffc3df4b9799568048bbe955c05dc
luqi <lq.xie.work@gmail.com>
2024-10-29 14:02:31 -0700

    complete wave4

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response,request
+from flask import Blueprint, abort, make_response,request, Response
 from app.models.planets import Planet
 from ..db import db
 # endpoint for reading all planets info
@@ -28,6 +28,34 @@ def get_all_planets():
 
   return planets_response, 200
 
+@planets_bp.get("/<planet_id>")
+def get_single_planet(planet_id):
+  planet = validate_planet(planet_id)
+
+  return planet.to_dict()
+
+@planets_bp.put("/<planet_id>")
+def update_planet(planet_id):
+  planet = validate_planet(planet_id)
+  request_body = request.get_json()
+
+  planet.name = request_body["name"]
+  planet.description = request_body["description"]
+  planet.random = request_body["random"]
+
+  db.session.commit()
+  return Response(status=204, mimetype='application/json')
+
+@planets_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+  planet = validate_planet(planet_id)
+
+  db.session.delete(planet)
+  db.session.commit()
+  return Response(status=204, mimetype='application/json')
+
+
+
 
 
 
@@ -47,16 +75,19 @@ def get_all_planets():
 #   return planet.to_dict(), 200
 
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"message": f"planet {planet_id} invalid"}
-#         abort(make_response(response, 400))
+def validate_planet(planet_id):
+  try:
+      planet_id = int(planet_id)
+  except:
+      response = {"message": f"planet {planet_id} invalid"}
+      abort(make_response(response, 400))
 
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
 
-#     response = {"message": f"planet {planet_id} not found"}
-#     abort(make_response(response, 404))
+  query = db.select(Planet).where(Planet.id == planet_id)
+  planet = db.session.scalar(query)
+
+  if not planet:
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
+
+  return planet
